0cfe95cc9c4e0f21347918e979ef6dbc0c8362ae
Ombito <alvinombito09@gmail.com>
2023-11-01 15:09:10 +0300

    Feat: add app

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 CORS(app)
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -110,3 +111,5 @@ api.add_resource(FriendRequestResource, '/friend_requests')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
